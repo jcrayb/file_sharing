b1661383c18d006d2f01f710391ba832eea3ef71
Chris <jcrb7800@gmail.com>
2023-09-12 19:52:29 -0500

    healthcheck

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__, static_folder = 'files')
 
+@app.route('/healthcheck')
+def healthcheck():
+    return {'status':'healthy'}
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port = '8080', debug=True)
+    app.run(host="0.0.0.0", port = '8080')
