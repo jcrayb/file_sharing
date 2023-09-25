352165886ab8a81de5c1f4823f7461cec6507530
jcrayb <jcrb7800@gmail.com>
2023-09-25 10:35:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,26 @@ app = Flask(__name__, static_folder = 'static')
 CORS(app)
 shared_folder = 'files'
 
-
 @app.route('/files', defaults={'path':''}, methods=['GET'])
 @app.route('/files/<path:path>')
 def route_get_files(path):
+    if not path:
+        return abort(404)
+    
     total_path = os.path.join(shared_folder, path)
 
     if not os.path.exists(total_path):
         return make_response('File doesn\'t exist', 404)
     
-    mime = guess_type(total_path)[0].split('/')
-    filetype = mime[0]
-    ext = mime[1]
+    try:
+        mime = guess_type(total_path)[0].split('/')
+        filetype = mime[0]
+        ext = mime[1]
+    except AttributeError:
+        data = open(total_path, 'r').read()
+        response = make_response(data, 200)
+        response.mimetype = 'text/plain'
+        return response
 
     if filetype == 'text':
         data = open(total_path, 'r').read()
